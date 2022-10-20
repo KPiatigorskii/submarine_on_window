2af65471b48fb6697252f5a76c4056d67ca9a100
KPiatigorskii <k.piatigorskii@gmail.com>
2022-10-20 16:54:29 +0300

    move sound vars in constants

--- submarine_on_window/constants.h
+++ submarine_on_window/constants.h
@@ -1,9 +1,17 @@
 #pragma once
 
-#define BACKGROUND_PATH (LPCWSTR)L"source/image/bg.bmp"
-#define HIT_CELL (LPCWSTR)L"source/image/square32cross.bmp"
-#define OPEN_EMPTY_CELL (LPCWSTR)L"source/image/square32white.bmp"
-#define OPEN_SHIP_CELL (LPCWSTR)L"source/image/square32green.bmp"
-#define CLOSE_CELL (LPCWSTR)L"source/image/square32red.bmp"
-#define MISS_CELL (LPCWSTR)L"source/image/square32dot.bmp"
+#define BACKGROUND_PATH TEXT("source/image/bg.bmp")
+
+#define HIT_CELL TEXT("source/image/square32cross.bmp")
+#define OPEN_EMPTY_CELL TEXT("source/image/square32white.bmp")
+#define OPEN_SHIP_CELL TEXT("source/image/square32green.bmp")
+#define CLOSE_CELL TEXT("source/image/square32red.bmp")
+#define MISS_CELL TEXT("source/image/square32dot.bmp")
+
+
+
+static TCHAR* intro_sound = TEXT("source/music/intro_sound.wav");
+static TCHAR* start_game_sound = TEXT("source/music/start_game.wav");
+static TCHAR* win_game_sound = TEXT("source/music/win_game.wav");
+static TCHAR* game_over_sound = TEXT("source/music/game_over.wav");
 	
